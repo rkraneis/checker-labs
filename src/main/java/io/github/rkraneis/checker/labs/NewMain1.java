e75e543a5f8e4ba4a7bd0ad3ca743859961fbd6f
René Kraneis <Rene.Kraneis@hays.de>
2016-03-07 15:49:39 +0100

    check for more interaction btwn regex and tainting

--- src/main/java/io/github/rkraneis/checker/labs/NewMain1.java
+++ src/main/java/io/github/rkraneis/checker/labs/NewMain1.java
@@ -18,8 +18,19 @@ public class NewMain1 {
         String s = args[0];
         foo(s); // should fail
         foo2(s);
+        int i = Integer.parseInt(s);
+        foo3(i); // should fail
+        int j = Integer.getInteger("possibly tainted property");
+        foo3(j); // should fail
+
+        String regex = "abc(\\d+)def";
+        System.out.println("abc123def".matches(regex));
+
+        String regex2 = "abc(\\d+def";
+//        System.out.println("abc123def".matches(regex2)); // always fails
     }
 
     private static void foo(@Untainted String s) {}
     private static void foo2(String s) {}
+    private static void foo3(@Untainted int i) {}
 }
